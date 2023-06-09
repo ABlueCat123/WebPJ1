6c1056853f93ed1387525312c7d8badc668dbb8c
liwenjun <714795301@qq.com>
2023-06-09 00:51:10 -0700

    修改记录类

--- SpringBoot/src/main/java/com/example/springboot/entity/Record.java
+++ SpringBoot/src/main/java/com/example/springboot/entity/Record.java
@@ -16,16 +16,16 @@ public class Record {
     private Long recordId;
 
     // 对局玩家1 可以直接规定玩家一就是警察身份 玩家二就是小偷身份 这样可以减少后端user类需要存储的属性
-    @ManyToOne(fetch = FetchType.LAZY,targetEntity = User.class,cascade = CascadeType.ALL)
-    @JoinColumn(name = "police")
-    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
-    private User police;
+//    @ManyToOne(fetch = FetchType.LAZY,targetEntity = User.class,cascade = CascadeType.ALL)
+//    @JoinColumn(name = "police")
+//    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private Long policeId;
 
     // 对局玩家2
-    @ManyToOne(fetch = FetchType.LAZY,targetEntity = User.class,cascade = CascadeType.ALL)
-    @JoinColumn(name = "thief")
-    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
-    private User thief;
+//    @ManyToOne(fetch = FetchType.LAZY,targetEntity = User.class,cascade = CascadeType.ALL)
+//    @JoinColumn(name = "thief")
+//    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private Long thiefId;
 
     // 对局开始的时间
     @DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss")
@@ -34,12 +34,6 @@ public class Record {
     // 对局时长
     private String time;
 
-    // 胜利玩家id
-    private Long winnerId;
-
     // 胜利玩家扮演的角色
     private Long winnerRole;
-
-    // 最终比分？ like 90:60
-    private String score;
 }
